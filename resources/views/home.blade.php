77413dfac4c84897a4baa543411ba562152c60bb
rchan007 <armandoroger007@gmail.com>
2025-03-21 16:26:49 +0000

    refactorizando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,46 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/home/style.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Proyecto K</title>
 </head>
 <body>
 <nav>
-            <div class="logo">
-                <a class="logo-title">PROYECTO K</a>
-            </div>
-            <ul>
-                <li><a href="">PROYECTO</a></li>
-                <li><a href="">UBICACION</a></li>
-                <li><a href="">DEPARTAMENTOS</a></li>
-                <li><a href="">PREGUNTAS FRECUENTES</a></li>
-            </ul>
-            <div class="contacto">
-                <a href="">CONTACTO</a>
-            </div>
-        </nav>
-    <div class="container">
-        <header>
-            <h1>DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</h1>
-        </header>
-        <article class="conoce_mas">
-            <section>
-                <p>Un nuevo <strong>estandar de vida</strong> en Quinta Jungle</p>
-            </section>
-            <section>
-                <a href="">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
-            </section>
-        </article>
+    <div class="logo">
+        <a class="logo-title">PROYECTO K</a>
     </div>
-    <article class=details_proyect>
-            <picture>
-                <img src="{{asset('img/edificio.jpg')}}" alt="">
-            </picture>
-            <section>
-                <p class="details_title">CARACTERISTICAS DEL PROYECTO</p>
-            </section>
-            <section>
-                <h1>ARQUITECTURA DE VANGUARDIA PARA ACOMPAÑAR TU ESTILO DE VIDA EXCLUSIVO SECTOR DE QUINTA JUNGE</h1>
-            </section>
-    </article>
+    <ul>
+        <li><a href="">PROYECTO</a></li>
+        <li><a href="">UBICACION</a></li>
+        <li><a href="">DEPARTAMENTOS</a></li>
+        <li><a href="">PREGUNTAS FRECUENTES</a></li>
+    </ul>
+    <div class="contacto">
+        <a href="">CONTACTO</a>
+    </div>
+</nav>
+<header>
+    <p>DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</p>
+    <div class="conoce_mas">
+        <a href="" class="texto-uno">Un nuevo <strong>estandar de vida</strong> en Quinta Jungle</a>
+        <a href="" class="texto-dos">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
+    </div>
+</header>
+<article class=details_proyect>
+    <picture>
+        <img src="{{asset('img/11d2dac1e2eeea4b1781b7f8abe72e2c.jpeg')}}" alt="">
+    </picture>
+    <section>
+        <p class="details_title">CARACTERISTICAS DEL PROYECTO</p>
+    </section>
+    <section>
+        <h1>ARQUITECTURA DE VANGUARDIA PARA ACOMPAÑAR TU ESTILO DE VIDA EXCLUSIVO SECTOR DE QUINTA JUNGE</h1>
+    </section>
+</article>
 </body>
 </html>

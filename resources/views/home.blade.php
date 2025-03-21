f19272e5bb99a7f6d2c2097e9a1774625021adcf
rchan007 <armandoroger007@gmail.com>
2025-03-21 17:33:04 +0000

    refactorizando la primera parte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,21 +25,23 @@
     </div>
 </nav>
 <header>
-    <p>DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</p>
-    <div class="conoce_mas">
-        <a href="" class="texto-uno">Un nuevo <strong>estandar de vida</strong> en Quinta Jungle</a>
-        <a href="" class="texto-dos">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
+    <p class="header-title">DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</p>
+    <div class="header-btns">
+        <a href="" class="header-text-one">Un nuevo <strong>estandar de vida</strong> en Quinta Jungle</a>
+        <div class="header-btn">
+            <a href="" class="header-text-two">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
+        </div>
     </div>
 </header>
-<article class=details_proyect>
+<article class=details-proyect>
     <picture>
         <img src="{{asset('img/11d2dac1e2eeea4b1781b7f8abe72e2c.jpeg')}}" alt="">
     </picture>
     <section>
-        <p class="details_title">CARACTERISTICAS DEL PROYECTO</p>
+        <p class="details-text-one">CARACTERISTICAS DEL PROYECTO</p>
     </section>
     <section>
-        <h1>ARQUITECTURA DE VANGUARDIA PARA ACOMPAÑAR TU ESTILO DE VIDA EXCLUSIVO SECTOR DE QUINTA JUNGE</h1>
+        <h1 class="details-text-two">ARQUITECTURA DE VANGUARDIA PARA ACOMPAÑAR TU ESTILO DE VIDA EXCLUSIVO SECTOR DE QUINTA JUNGE</h1>
     </section>
 </article>
 </body>

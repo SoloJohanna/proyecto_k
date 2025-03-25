c5729028316d86d5cce8fb98c05f0b47326608be
rogerarmandochanac <armandoroger007@gmail.com>
2025-03-25 17:53:32 +0000

    Seccion corregida de desborde y refactorizacion de codigo

--- resources/views/snipets/encabezado.blade.php
+++ resources/views/snipets/encabezado.blade.php
@@ -1,11 +1,11 @@
 <div class="encabezado">
     <div class="encabezado-grupo">
         <p class="encabezado-titulo">DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</p>
-        <div class="header-btns">
-            <a href="" class="header-text-one">Un nuevo <strong>estandar de vida </strong> en 📍Quinta Jungle</a>
-            <div class="header-btn">
-                <a href="" class="header-text-two">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
-                <div class="bottom-row">
+        <div class="encabezado-botones">
+            <a href="" class="encabezado-boton-uno">Un nuevo <strong>estandar de vida </strong> en 📍Quinta Jungle</a>
+            <div class="encabezado-boton-dos">
+                <a href="" class="encabezado-boton-dos-texto">¡OPORTUNIDAD DE COMPRA EN BLANCO! CONOCE MAS SOBRE EL PROYECTO K</a>
+                <div class="button-row">
                     <svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.3102 9.16607L8.19906 16.2772M8.19906 16.2772L1.08795 9.16607M8.19906 16.2772V2.05496" stroke="white" stroke-width="1.33333"/>
                     </svg>

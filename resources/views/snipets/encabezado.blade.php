54f6fe191884f204906a914a88f7f140e11d54a3
rogerarmandochanac <armandoroger007@gmail.com>
2025-03-26 23:48:21 +0000

    boton mailto en el encabezado listo

--- resources/views/snipets/encabezado.blade.php
+++ resources/views/snipets/encabezado.blade.php
@@ -3,9 +3,9 @@
         <p class="encabezado-titulo">DISEÑO INTELIGENTE, EFICIENCIA Y ARMONIA CON EL ENTORNO</p>
         <div class="encabezado-botones">
             <a href="" class="encabezado-boton-uno">Un nuevo <strong>estandar de vida </strong> en 📍Quinta Jungle</a>
-            <a href="" class="encabezado-boton-dos">
+            <a href="mailto:elcorreoquequieres@correo.com" class="encabezado-boton-dos">
                 <strong>¡OPORTUNIDAD DE COMPRA EN BLANCO!</strong> CONOCE MAS SOBRE EL PROYECTO K
-                <svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg class="encabezado-flecha-abajo" width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.3102 9.16607L8.19906 16.2772M8.19906 16.2772L1.08795 9.16607M8.19906 16.2772V2.05496" stroke="white" stroke-width="1.33333"/>
                 </svg>                
             </a>

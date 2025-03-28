18d35da22c4ad880f573682a6831e1d699cb870f
rogerarmandochanac <armandoroger007@gmail.com>
2025-03-28 13:52:33 +0000

    seccion espacios corregido

--- resources/views/snipets/espacios.blade.php
+++ resources/views/snipets/espacios.blade.php
@@ -1,6 +1,7 @@
-<div class="espacios-section">
+<div class="espacios">
     <div class="espacios-section-frame">
-        <p class="espacios-section-frame-title">EXPLORA LOS ESPACIOS QUE TE RODEAN</p>
+        <p class="espacios-title">Explora los espacios
+        que te esperan</p>
     </div>
     <img src="{{asset('img/salon_multihusos.jpeg')}}" alt="">
 </div>

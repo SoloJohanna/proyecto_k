68d89476ae93c871ea95b037be5556fd719f8744
rogerarmandochanac <armandoroger007@gmail.com>
2025-03-29 22:20:23 +0000

    correccion de listas de precios

--- resources/views/snipets/navbar_plantas.blade.php
+++ resources/views/snipets/navbar_plantas.blade.php
@@ -1,13 +1,10 @@
 <nav class="fondo-negro">
-    <div class="nav-logo">
-        <a href="{{url('')}}" class="nav-logo-title color-blanco">PROYECTO K</a>
-    </div>
+    <a href="{{url('')}}" class="nav-logo color-blanco">PROYECTO K</a>
     <ul class="nav-list">
         <li><a href="#caracteristicas" class="nav-list-item color-blanco">PROYECTO</a></li>
         <li><a href="#location" class="nav-list-item color-blanco">UBICACIÓN</a></li>
         <li><a href="#departments" class="nav-list-item color-blanco">DEPARTAMENTOS</a></li>
     </ul>
-    <div class="nav-contacto">
-        <a class="nav-contacto-item color-blanco" href="mailto:carolina@quintajungepropiedades.cl">CONTACTO</a>
-    </div>
+
+    <a class="nav-item color-blanco" href="mailto:carolina@quintajungepropiedades.cl">CONTACTO</a>
 </nav>

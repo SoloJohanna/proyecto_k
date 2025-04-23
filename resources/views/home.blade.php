0615520e603f2fc7151b62bfefebfae7d4aa5104
rogerarmandochanac <armandoroger007@gmail.com>
2025-04-23 11:18:12 -0600

    navbar better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/home/style.css')}}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="sweetalert2.min.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <script src="{{asset('js/home/script.js')}}"></script>
-    <title>Proyecto K</title>
-</head>
-<body>
+@extends('base')
+  @section('content')
     @include('snipets.navbar', ['link_proyecto'=>'#caracteristicas', 'link_ubicacion'=>'#location', 'link_departamentos'=>'#departments', 'fondo_negro'=>'none', "texto_blanco"=>'none', 'nav_list'=>'nav-list'])
     @include('snipets.encabezado')
     @include('snipets.caracteristicas')
@@ -23,31 +10,10 @@
     @include('snipets.contacto')
     @include('snipets.schedules')
     @include('snipets.footer', ['link_proyecto'=>'#caracteristicas', 'link_ubicacion'=>'#location', 'link_departamentos'=>'#departments'])
-    <!-- Meta Pixel Code -->
-    <script src="{{asset('js/slider.js')}}"></script>
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '998482552390703');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=998482552390703&ev=PageView&noscript=1"
-/></noscript>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R9VZLWC882"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-R9VZLWC882');
+  @endsection
+  @section('script')
+  <script src="{{asset('js/home/script.js')}}"></script>
+  @endsection
 </script>
 </body>
 </html>

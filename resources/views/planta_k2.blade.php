f3205ca77cb64dbf5ffcbb5a9306fd0a66f6934c
Nicolas Oyarce <nicoyarce@gmail.com>
2025-05-25 01:41:55 -0400

    Change buttons in plantas to send whatsapp message

--- resources/views/planta_k2.blade.php
+++ resources/views/planta_k2.blade.php
@@ -1,14 +1,25 @@
 @extends('base')
 @section('content')
-@include('snipets.navbar_plantas', ['link_proyecto'=>'#caracteristicas-pisos', 'link_ubicacion'=>'#schedules', 'link_departamentos'=>'#equipment-plantas', 'fondo_negro'=>'fondo-negro', "texto_blanco"=>'texto-blanco', 'nav_list'=>'nav-list-negro'])
+    @include('snipets.navbar_plantas', [
+        'link_proyecto' => '#caracteristicas-pisos',
+        'link_ubicacion' => '#schedules',
+        'link_departamentos' => '#equipment-plantas',
+        'fondo_negro' => 'fondo-negro',
+        'texto_blanco' => 'texto-blanco',
+        'nav_list' => 'nav-list-negro',
+    ])
     @include('snipets.encabezado_plantas')
-    @include('snipets.floor_k2')
+    @include('snipets.floor_k2', ['whatsapp_url' => $whatsapp_url])
     @include('snipets.espacios')
     @include('snipets.equipment_plantas')
     @include('snipets.contacto')
     @include('snipets.schedules')
-    @include('snipets.footer_plantas', ['link_proyecto'=>'#caracteristicas-pisos', 'link_ubicacion'=>'#schedules', 'link_departamentos'=>'#equipment-plantas'])
+    @include('snipets.footer_plantas', [
+        'link_proyecto' => '#caracteristicas-pisos',
+        'link_ubicacion' => '#schedules',
+        'link_departamentos' => '#equipment-plantas',
+    ])
 @endsection
 @section('script')
-<script src="{{asset('js/pisos/script.js')}}"></script>
+    <script src="{{ asset('js/pisos/script.js') }}"></script>
 @endsection

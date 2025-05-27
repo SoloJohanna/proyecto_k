2583935e1249281d5a3db042aa5876823a15957a
Nicolas Oyarce <nicoyarce@gmail.com>
2025-05-27 00:41:45 -0400

    Refactor button structure in equipamiento and adjust grayscale filter in schedules for improved design consistency

--- resources/views/snipets/schedules.blade.php
+++ resources/views/snipets/schedules.blade.php
@@ -1,7 +1,10 @@
 <div class="schedules contenedor" id="schedules">
-    <p><strong>atención de ventas:</strong><br> 
-    lunes a sábado: 10:00 a 14:00 y 15:30 a 18:00 hrs.</p>
+    <p><strong>atención de ventas:</strong><br>
+        lunes a sábado: 10:00 a 14:00 y 15:30 a 18:00 hrs.</p>
     <picture>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1639.9920380246056!2d-73.0443293439786!3d-36.850267823853116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2scl!4v1743090883956!5m2!1ses-419!2scl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6385.157621052055!2d-73.04775102516174!3d-36.85256212227728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6038c644c3be4257%3A0x799caf8d5bd65431!2sProyecto%20K!5e0!3m2!1ses-419!2scl!4v1748299673566!5m2!1ses-419!2scl"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </picture>
 </div>

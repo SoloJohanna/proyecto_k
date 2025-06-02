7819b4820e23e3341b93f513f4e0a149b187ef80
Nicolas Oyarce <nicoyarce@gmail.com>
2025-06-02 02:10:30 -0400

    Update navbar links to use route helper for consistency and improved maintainability

--- resources/views/snipets/navbar.blade.php
+++ resources/views/snipets/navbar.blade.php
@@ -5,15 +5,15 @@
     <!-- NAVEGACIÓN DESKTOP -->
     <div class="nav__desktop">
         <ul class="nav__menu">
-            <li><a href="{{ url('/#caracteristicas') }}" class="nav__link">PROYECTO</a></li>
-            <li><a href="{{ url('/#location') }}" class="nav__link">UBICACIÓN</a></li>
-            <li><a href="{{ url('/#schedules') }}" class="nav__link">ENTORNO</a></li>
-            <li><a href="{{ url('/#departments') }}" class="nav__link">DEPARTAMENTOS</a></li>
+            <li><a href="{{ route('home') }}#caracteristicas" class="nav__link">PROYECTO</a></li>
+            <li><a href="{{ route('home') }}#location" class="nav__link">UBICACIÓN</a></li>
+            <li><a href="{{ route('home') }}#schedules" class="nav__link">ENTORNO</a></li>
+            <li><a href="{{ route('home') }}#departments" class="nav__link">DEPARTAMENTOS</a></li>
         </ul>
     </div>
 
     <!-- CONTACTO SEPARADO -->
-    <a href="#contacto" class="nav__contacto">CONTACTO</a>
+    <a href="{{ route('home') }}#contacto" class="nav__contacto">CONTACTO</a>
 
     <!-- HAMBURGER MENU -->
     <div class="nav__hamburger" onclick="toggleMenu()">
@@ -27,19 +27,19 @@
 <div class="nav__mobile" id="mobileMenu">
     <ul class="nav__mobile__menu">
         <li class="nav__mobile__item">
-            <a href="#caracteristicas" class="nav__mobile__link" onclick="closeMenu()">PROYECTO</a>
+            <a href="{{ route('home') }}#caracteristicas" class="nav__mobile__link" onclick="closeMenu()">PROYECTO</a>
         </li>
         <li class="nav__mobile__item">
-            <a href="#location" class="nav__mobile__link" onclick="closeMenu()">UBICACIÓN</a>
+            <a href="{{ route('home') }}#location" class="nav__mobile__link" onclick="closeMenu()">UBICACIÓN</a>
         </li>
         <li class="nav__mobile__item">
-            <a href="#schedules" class="nav__mobile__link" onclick="closeMenu()">ENTORNO</a>
+            <a href="{{ route('home') }}#schedules" class="nav__mobile__link" onclick="closeMenu()">ENTORNO</a>
         </li>
         <li class="nav__mobile__item">
-            <a href="#departments" class="nav__mobile__link" onclick="closeMenu()">DEPARTAMENTOS</a>
+            <a href="{{ route('home') }}#departments" class="nav__mobile__link" onclick="closeMenu()">DEPARTAMENTOS</a>
         </li>
         <li class="nav__mobile__item">
-            <a href="#contacto" class="nav__mobile__link" onclick="closeMenu()">CONTACTO</a>
+            <a href="{{ route('home') }}#contacto" class="nav__mobile__link" onclick="closeMenu()">CONTACTO</a>
         </li>
     </ul>
 
